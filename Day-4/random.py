6c7fd3fe6f591fe9be947f91da2edfa5178e22e9
Shaswat Raj <shaswatraj3@gmail.com>
2024-06-12 11:48:03 +0000

    random

---
+++ Day-4/random.py
@@ -0,0 +1,15 @@
+import random
+# https://www.askpython.com/python/examples/generating-random-numbers-python
+
+print(random.randint(1,1000))
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day-4/toss.py
@@ -0,0 +1,7 @@
+import random
+
+
+if(random.random()< 0.5):
+    print("Heads")
+else:
+    print("Tails")
